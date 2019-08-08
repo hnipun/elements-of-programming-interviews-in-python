fa4b3436ab2eb455d791535dfb53223405666414
hnipun <hnipun@gmail.com>
2019-08-08 08:41:19 +0530

    added pre order traversal

--- Binary Trees/tree_traversal(loop).py
+++ Binary Trees/tree_traversal(loop).py
@@ -17,18 +17,13 @@ def inOrder(root):
     done = 0
 
     while (not done):
-
         # Reach the left most Node of the current Node
         if current is not None:
-
             # Place pointer to a tree node on the stack
             # before traversing the node's left subtree
             s.append(current)
-
             current = current.left
-
-
-            # BackTrack from the empty subtree and visit the Node
+        # BackTrack from the empty subtree and visit the Node
         # at the top of the stack; however, if the stack is
         # empty you are done
         else:
@@ -39,13 +34,38 @@ def inOrder(root):
                 # We have visited the node and its left
                 # subtree. Now, it's right subtree's turn
                 current = current.right
+            else:
+                done = 1
+                
+# Iterative function for inorder tree traversal
+def preOrder(root):
+    # Set current to root of binary tree
+    current = root
+    s = []  # initialze stack
+    done = 0
 
+    while (not done):
+        # Reach the left most Node of the current Node
+        if current is not None:
+            # Place pointer to a tree node on the stack
+            # before traversing the node's left subtree
+            s.append(current)
+            print (current.data,)
+            current = current.left
+        # BackTrack from the empty subtree and visit the Node
+        # at the top of the stack; however, if the stack is
+        # empty you are done
+        else:
+            if (len(s) > 0):
+                current = s.pop()
+                # We have visited the node and its left
+                # subtree. Now, it's right subtree's turn
+                current = current.right
             else:
                 done = 1
 
 
 # Driver program to test above function
-
 """ Constructed binary tree is 
             1 
           /   \ 
@@ -60,5 +80,5 @@ root.left.left = Node(4)
 root.left.right = Node(5)
 
 inOrder(root)
-
+preOrder(root)
 # This code is contributed by Nikhil Kumar Singh(nickzuck_007)
